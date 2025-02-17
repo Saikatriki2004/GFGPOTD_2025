5a313897ee520a2ce0e3c29d4301dcedb2e5479e
Saikat Rana <149765443+Saikatriki2004@users.noreply.github.com>
2025-02-17 08:59:43 +0530

    k largest elements.

--- February_2025/Feb_17.py
+++ February_2025/Feb_17.py
@@ -5,3 +5,4 @@ class Solution:
     def kLargest(self, arr: List[int], k: int) -> List[int]:
         # heapq.nlargest returns the k largest elements in descending order.
         return heapq.nlargest(k, arr)
+#Have a Good Day!
